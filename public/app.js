b3a7b6cc9f4930a4e8691ef13b24dda2039c4f4d
andrew guppy <andrew.guppy@blueyonder.co.uk>
2017-02-16 09:16:33 +0000

    Week11 Day3 Homework Spotify Ajax Style- pull back all images in artist for an album git push -u origin master

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ var populateList = function(albumsToPrint, poptype) {
 
   albumsToPrint.forEach(function(album) {
   var p = document.createElement('p');
-  var img = document.createElement('img');
+  
 
     if(poptype == "album"){
         p.innerText = "Album title: " + album.name + "\nArtist Name: " + album.artists[0].name + "\nAlbum Type: " + album.album_type;
@@ -22,11 +22,16 @@ var populateList = function(albumsToPrint, poptype) {
       }
     
     if(poptype == "artist"){
-        p.innerText = "Album title: " + album.name + "\nAlbum Genre: " + album.genres ;
-        img.src = album.images[0].url;
-        img.style = "width:315px;height:315px";
+        p.innerText = "Album title: " + album.name + "\nAlbum Genre: " + album.genres;
         list.appendChild(p);
-        list.appendChild(img);
+        album.images.forEach(function(image){
+        var img = document.createElement('img');
+        img.src = image.url;
+        img.style = "width:" + (image.width/3.5) + "px;height:" + (image.height/3.5) + "px" ;
+        list.appendChild(img);          
+        });
+
+        
       }       
   });
 }
@@ -42,19 +47,21 @@ var getAlbumsByArtist = function() {
 
 var requestComplete = function() {
   if (this.status != 200) return;
-  var retString = this.responseText;
 
-  if (retString.substring(5, 11) == "albums") {
-    var requestedData = JSON.parse(retString);
+  var retString = this.responseText;
+  var retType = retString.substring(5,11)
+  var requestedData = JSON.parse(retString);
+
+  if (retType == "albums") {
     albums = requestedData.albums.items;
     populateList(albums, "album");
-  };
+    };
 
-  if (retString.substring(5, 11) == "artist") {
-    var requestedData = JSON.parse(retString);
+  if (retType == "artist") {
+    console.log(retString);
     albums = requestedData.artists.items;
     populateList(albums, "artist");
-  };
+    };
 
   
 }
